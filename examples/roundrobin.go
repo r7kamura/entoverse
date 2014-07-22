8b2f7687de2cbfd73036737100965c38e1b34055
Ryo Nakamura <r7kamura@gmail.com>
2014-07-23 01:45:34 +0900

    Update examples

--- examples/roundrobin.go
+++ examples/roundrobin.go
@@ -1,41 +1,3 @@
-# Entoverse
-A library to implement host-based HTTP reverse-proxy in Golang.
-
-## Usage
-Here are some example uses of entoverse.
-
-### Example1 - single host
-`:3000` -> `:4000`
-
-```go
-package main
-
-import(
-	"log"
-	"net/http"
-	"github.com/r7kamura/entoverse"
-)
-
-func main() {
-	// Please implement your host converter function.
-	// This example always delegates HTTP requests to localhost:4000.
-	hostConverter := func(originalHost string) string {
-		return "localhost:4000"
-	}
-
-	// Creates an entoverse.Proxy object as an HTTP handler.
-	proxy := entoverse.NewProxy(hostConverter)
-
-	// Runs a reverse-proxy server on http://localhost:3000/
-	serverError := http.ListenAndServe("localhost:3000", proxy)
-	log.Fatal(serverError)
-}
-```
-
-### Example2 - multi hosts
-`:3000` -> `:4000, :5000, :6000`
-
-```go
 package main
 
 import(
@@ -74,4 +36,3 @@ func main() {
 	serverError := http.ListenAndServe(":3000", proxy)
 	log.Fatal(serverError)
 }
-```
